ff0be1cc4cbcc64fb563b273d276904a191dd7e3
alanysouza <2021345@student.cct.ie>
2022-11-14 13:56:48 +0000

    CreatingFiles

---
+++ index.js
@@ -0,0 +1,27 @@
+const   http = require('http'), //HTTP server
+        express = require ('express'), //handling HTTP requests and routing
+        fs = require('fs'), //file system functionalities
+        xmlParse = require ('xslt-processor').xmlParse, //XML handling
+        xsltProcess =require('xslt-processor').xsltProcess, //XSLT handling
+        router = express(), //Init our router
+        server = http.createServer(router); //Init our server
+router.get('/', function(req, res) {
+    
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('menu.xml', 'utf8'),
+        xsl = fs.readFileSync('menu.xml', 'utf8');
+
+    xml = xmlParse(xml);
+    xsl = xmlParse(xsl);
+
+    let html = xsltProcess(xml, xsl);
+
+    res.end(html.toString());
+})
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
